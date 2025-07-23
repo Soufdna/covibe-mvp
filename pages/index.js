ad49b0b2f22b9f7d379af52d9946a89a7e41114e
Soufiane El Hattab <ton.email@exemple.com>
2025-07-23 15:30:57 -0700

    fix: correct next-i18next import path

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import { useTranslation } from 'next-i18next';
-import { serverSideTranslations } from 'next-i18Next/serverSideTranslations';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import nextI18NextConfig from '../next-i18next.config';
 import ProfileCard from '../components/ProfileCard';
